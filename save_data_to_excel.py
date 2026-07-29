48400bead285f267ce32665be4987bd2ba1a3fe4
Sushant Bhattarai <sushantb2055@gmail.com>
2026-07-29 14:37:49 -0400

    data obtained: RANDOM graphs rebuttal experiments

--- save_data_to_excel.py
+++ save_data_to_excel.py
@@ -74,7 +74,7 @@ def save_error_stretch_to_excel(fractions, max_errors, min_errors, stretches, st
     summary_df = pd.DataFrame(summary_records)
 
     # Ensure output directory exists
-    folder = "./results/grid_graphs/"
+    folder = "./rebuttal_results/random_graphs/third/"
     os.makedirs(folder, exist_ok=True)
     excel_path = os.path.join(folder, f"{os.path.splitext(file_name)[0]}.xlsx")
 
